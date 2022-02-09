06fc001fb0f7492065b87fdf9fa33205a59e05cd
jcsgh <business.joshuac@gmail.com>
2022-02-09 13:50:49 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import logo from './logo.svg';
-import {Component} from 'react';
+import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
 
+  state = {
+    data: [],
+    question: '',
+    value: '',
+    category: '',
+    score: ''
+  }
+
+  handleClick = () => {
+    fetch("http://jservice.io/api/random")
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .then(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
-        <h1>Jeopardy</h1>
+        <h1>Welcome to Jeopardy!</h1>
+
+        <button onClick={this.handleClick}>Get question</button>
+
+        <div>
+
+        </div>
       </div>
     );
   }
-  
+
 }
 
 export default App;
