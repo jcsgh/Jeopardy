ebc8370c3c06ef1e5916696977832b0e5d4f55b6
jcsgh <business.joshuac@gmail.com>
2022-02-09 15:39:19 -0600

    got some things to work

--- src/App.js
+++ src/App.js
@@ -1,38 +1,85 @@
-import logo from './logo.svg';
 import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
 
   state = {
-    data: [],
-    question: '',
-    value: '',
-    category: '',
-    score: ''
+    question: null,
+    category: null,
+    answer: null
   }
 
-  handleClick = () => {
-    fetch("http://jservice.io/api/random")
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .then(error => console.log(error))
+  handleClick = async () => {
+    try {
+      const response = await fetch("http://jservice.io/api/random")
+      const data = await response.json()
+      this.setState({
+        question: data[0],
+        category: data[0],
+        answer: data[0]
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>Welcome to Jeopardy!</h1>
+        <header>
+          <h1>Welcome to Jeopardy!</h1>
+        </header>
+
 
         <button onClick={this.handleClick}>Get question</button>
 
-        <div>
-
+        <h2>Question:</h2>
+        <div className='question'>
+          {this.state.question && <RandomQuestion randomQuestion={this.state.question} />}
         </div>
+        <h2>Category:</h2>
+        <div className='category'>
+          {this.state.category && <Category category={this.state.category} />}
+        </div>
+        <h2>Answer:</h2>
+        <div className='answer'>
+          {this.state.answer && <Answer answer={this.state.answer} />}
+        </div>
+
       </div>
     );
   }
 
 }
+
+const RandomQuestion = (props) => {
+  const { randomQuestion } = props
+  console.log(randomQuestion)
+  return (
+    <div className="question">
+      <p>{randomQuestion.question}</p>
+    </div>
+  )
+}
+
+const Category = (props) => {
+  const { category } = props
+  return (
+    <div className="category">
+      <p>{category.title}</p>
+    </div>
+  )
+}
+
+const Answer = (props) => {
+  const { answer } = props
+  return (
+    <div className="answer">
+      <p>{answer.answer}</p>
+    </div>
+  )
+}
+
+
 
 export default App;
